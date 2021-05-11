9430868e7ec5efc1f7a1b90eb1e3b55ba8106da7
김종환 <dondon9513@gmail.com>
2021-05-11 16:52:04 +0900

    Habit tracker 4.17

--- habit-tracker/src/components/habitAddForm.jsx
+++ habit-tracker/src/components/habitAddForm.jsx
@@ -1,32 +1,30 @@
-import React, { PureComponent } from "react";
+import React from "react";
 
-class HabitAddForm extends PureComponent {
-  formRef = React.createRef();
-  inputRef = React.createRef();
-  onSubmit = (event) => {
+const HabitAddForm = (props) => {
+  const formRef = React.createRef();
+  const inputRef = React.createRef();
+
+  const onSubmit = (event) => {
     event.preventDefault(); //페이지가 리로딩되는것을 방지해줌
-    const name = this.inputRef.current.value;
-    name && this.props.onAdd(name);
+    const name = inputRef.current.value;
+    name && props.onAdd(name);
     // this.inputRef.current.value = "";
     this.formRef.current.reset();
   };
 
-  render() {
-    console.log("dddd");
-    return (
-      <div>
-        <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-          <input
-            ref={this.inputRef}
-            type="text"
-            className="add-input"
-            placeholder="Habit"
-          />
-          <button className="add-button">Add</button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+        <input
+          ref={inputRef}
+          type="text"
+          className="add-input"
+          placeholder="Habit"
+        />
+        <button className="add-button">Add</button>
+      </form>
+    </div>
+  );
+};
 
 export default HabitAddForm;
