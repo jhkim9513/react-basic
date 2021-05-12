1fc5e99a2a828ab4988b627074d03f97d6e67fd4
김종환 <dondon9513@gmail.com>
2021-05-12 19:29:16 +0900

    youtube fetch api start

---
+++ youtube/src/app.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./components/video_list/video_list";
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCcQrTa8e9LRFeUcc2360vB9zlIAcb5erc",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos} />;
+}
+
+export default App;
