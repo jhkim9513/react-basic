87d2cf822335c4094b9ac51a3c1f35faed6b2420
김종환 <dondon9513@gmail.com>
2021-05-14 17:42:34 +0900

    fixed duplicate id when searched

--- youtube/src/app.jsx
+++ youtube/src/app.jsx
@@ -5,6 +5,25 @@ import VideoList from "./components/video_list/video_list";
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyCcQrTa8e9LRFeUcc2360vB9zlIAcb5erc`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        //유행하는 영상을 받아올 때와 달리 검색했을 시에는 id가 object형식으로 들어오기 때문에
+        //map을 사용하여 id를 object안에 있는 videoId로 다 덮어주어 새로운 item을 setVideo에 넘긴다
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log("error", error));
+  };
 
   useEffect(() => {
     const requestOptions = {
@@ -23,7 +42,7 @@ function App() {
 
   return (
     <div className={styles.app}>
-      <SearchHeader />
+      <SearchHeader onSearch={search} />
       <VideoList videos={videos} />
     </div>
   );
