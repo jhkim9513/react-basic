c8938b7662a135bd470f297cc8778f01a1799859
김종환 <dondon9513@gmail.com>
2021-05-17 17:47:44 +0900

    add youtube service

---
+++ youtube/src/service/youtube.js
@@ -0,0 +1,39 @@
+class Youtube {
+  constructor(key) {
+    this.key = key;
+    this.getRequestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+  }
+
+  async mostPopular() {
+    try {
+      const response = await fetch(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+        this.getRequestOptions
+      );
+      const result = await response.json();
+      return result.items;
+    } catch (error) {
+      return console.log("error", error);
+    }
+  }
+
+  async search(query) {
+    try {
+      const response = await fetch(
+        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
+        this.getRequestOptions
+      );
+      const result = await response.json();
+      //유행하는 영상을 받아올 때와 달리 검색했을 시에는 id가 object형식으로 들어오기 때문에
+      //map을 사용하여 id를 object안에 있는 videoId로 다 덮어주어 새로운 item을 setVideo에 넘긴다
+      return result.items.map((item) => ({ ...item, id: item.id.videoId }));
+    } catch (error) {
+      return console.log("error", error);
+    }
+  }
+}
+
+export default Youtube;
