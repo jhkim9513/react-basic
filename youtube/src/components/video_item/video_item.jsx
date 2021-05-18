ea4f22e7773950a2fdeb48ee596cd7326a3e9fb8
김종환 <dondon9513@gmail.com>
2021-05-18 17:43:29 +0900

    change to PureComponent

--- youtube/src/components/video_item/video_item.jsx
+++ youtube/src/components/video_item/video_item.jsx
@@ -1,28 +1,30 @@
-import React from "react";
+import React, { memo } from "react";
 import styles from "./video_item.module.css";
 
-const VideoItem = (
-  { video, video: { snippet }, onVideoClick, display } //props.video.snippet
-) => {
-  const displayType = display === "list" ? styles.list : styles.grid;
-  return (
-    <li
-      className={`${styles.container} ${displayType}`}
-      onClick={() => onVideoClick(video)}
-    >
-      <div className={styles.video}>
-        <img
-          className={styles.thumbnail}
-          src={snippet.thumbnails.medium.url}
-          alt="video thumbnail"
-        />
-        <div className={styles.metadata}>
-          <p className={styles.title}>{snippet.title}</p>
-          <p className={styles.channel}>{snippet.channelTtitle}</p>
+const VideoItem = memo(
+  (
+    { video, video: { snippet }, onVideoClick, display } //props.video.snippet
+  ) => {
+    const displayType = display === "list" ? styles.list : styles.grid;
+    return (
+      <li
+        className={`${styles.container} ${displayType}`}
+        onClick={() => onVideoClick(video)}
+      >
+        <div className={styles.video}>
+          <img
+            className={styles.thumbnail}
+            src={snippet.thumbnails.medium.url}
+            alt="video thumbnail"
+          />
+          <div className={styles.metadata}>
+            <p className={styles.title}>{snippet.title}</p>
+            <p className={styles.channel}>{snippet.channelTtitle}</p>
+          </div>
         </div>
-      </div>
-    </li>
-  );
-};
+      </li>
+    );
+  }
+);
 
 export default VideoItem;
